949d28fe63966ecad4b6556c82d4a4e789e804db
ItsMeLin <147966712+ItsMeLin@users.noreply.github.com>
2023-10-25 10:38:02 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -72,8 +72,6 @@ def main():
         cnc = input('''@Lintar\n ==>''')
         if cnc == "Methods" or cnc == "METHODS" or cnc == "methods":
             methods()
-	elif cnc == "Clear" or cnc == "CLEAR" or cnc == "clear":
-	    logo()
 		
 # LAYER 7 METHODS
                 
